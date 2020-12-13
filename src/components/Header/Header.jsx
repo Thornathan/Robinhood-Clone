e702ab60f9eef736f05cb9300069cea696ae457a
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-12-13 02:49:50 -0800

    Finished Header template

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,31 @@
 import "./Header.css";
+import Logo from "../../robinhood.svg";
 
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>
+    <div className="header__wrapper">
       {/*  logo  */}
+      <div className="header__logo">
+        <img src={Logo} width={25} alt="" />
+      </div>
+      {/* search */}
+      <div className="header__search">
+        <div className="header__searchContainer">
+          <input placeholder="Search" type="text" />
+        </div>
+        {/* menu */}
+        <div className="header__menuItems">
+          <a href="#">Free Stocks</a>
+          <a href="">Portfolio</a>
+          <a href="">Cash</a>
+          <a href="">Messages</a>
+          <a href="">Account</a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
